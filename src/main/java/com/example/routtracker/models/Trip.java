c6f1b44f4a517e339234fba3aac2e086101c8467
Marek Kożuchowski <marekkozuchowski4@gmail.com>
2022-11-11 10:55:50 +0100

    modified typ of amount in Trip model

--- src/main/java/com/example/routtracker/models/Trip.java
+++ src/main/java/com/example/routtracker/models/Trip.java
@@ -3,10 +3,12 @@ package com.example.routtracker.models;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.math.BigDecimal;
+
 @Data
 @AllArgsConstructor
 public class Trip {
     private String originAddress;
     private String destinationAddress;
-    private double amount;
+    private BigDecimal amount;
 }
