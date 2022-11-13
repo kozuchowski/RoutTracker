e0c52c74a41e6480c1b616c5276ff866d2bc1ba1
Marek Kożuchowski <marekkozuchowski4@gmail.com>
2022-11-13 21:02:55 +0100

    changed service to repository

--- src/main/java/com/example/routtracker/repository/TripRepository.java
+++ src/main/java/com/example/routtracker/repository/TripRepository.java
@@ -3,6 +3,7 @@ package com.example.routtracker.repository;
 import com.example.routtracker.model.Trip;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -10,12 +11,12 @@ import java.util.Optional;
 @Repository
 public interface TripRepository extends JpaRepository<Trip, Long> {
 
-    void addTrip(Trip trip);
 
-    List<Trip> findAllById(Long id);
+    List<Trip> findAll();
 
     Optional<Trip> findById(Long id);
 
+    @Transactional
     void deleteTripById(Long id);
 
 
